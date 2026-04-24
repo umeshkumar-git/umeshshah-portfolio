2707b170e6278391de56146d4f8837cf15448970
umeshkumar-git <umeshkumarshah460@gmail.com>
2026-04-24 20:39:08 +0530

    delete extra files

--- src/shims/webgl-sdf-generator 2.js
+++
@@ -1,6 +0,0 @@
-// src/shims/webgl-sdf-generator.js
-export default function createSDFGenerator() {
-	console.warn("Using local webgl-sdf-generator shim");
-}
-// You might also need to export it as a named export depending on the version
-export { createSDFGenerator };
